b46d27995941d12f63603fad78ccb099318f9bc1
SUHAYB nur kalif <zaakcunaaye@gmail.com>
2026-09-20 13:24:31 +0300

    Add service worker for caching and fetch handling
    Implement service worker for caching app files and handling fetch requests.

---
+++ service-worker.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = "the-noor-cache-v1";
+
+const APP_FILES = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(APP_FILES);
+    })
+  );
+
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((cacheName) => cacheName !== CACHE_NAME)
+          .map((cacheName) => caches.delete(cacheName))
+      );
+    })
+  );
+
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedFile) => {
+      return cachedFile || fetch(event.request);
+    })
+  );
+});
